b37947fa1f712921973996682278235e91296344
madhavsharma0070-ctrl <madhavsharma0070@gmail.com>
2026-03-09 16:28:31 +0530

    Create MenuContext.tsx

---
+++ components/components/MenuContext.tsx
@@ -0,0 +1,39 @@
+‘use client’;
+
+import { createContext, useContext, useState, ReactNode } from ‘react’;
+
+interface MenuContextType {
+isOpen: boolean;
+openMenu: () => void;
+closeMenu: () => void;
+}
+
+const MenuContext = createContext<MenuContextType>({
+isOpen: false,
+openMenu: () => {},
+closeMenu: () => {},
+});
+
+export function MenuProvider({ children }: { children: ReactNode }) {
+const [isOpen, setIsOpen] = useState(false);
+
+const openMenu = () => {
+setIsOpen(true);
+document.body.style.overflow = ‘hidden’;
+};
+
+const closeMenu = () => {
+setIsOpen(false);
+document.body.style.overflow = ‘’;
+};
+
+return (
+<MenuContext.Provider value={{ isOpen, openMenu, closeMenu }}>
+{children}
+</MenuContext.Provider>
+);
+}
+
+export function useMenu() {
+return useContext(MenuContext);
+}
