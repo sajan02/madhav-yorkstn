2c3f137b34418c56e9609aa7b1c011a130378ef7
madhavsharma0070-ctrl <madhavsharma0070@gmail.com>
2026-03-09 16:39:54 +0530

    Create Footer.tsx

---
+++ components/components/Footer.tsx
@@ -0,0 +1,50 @@
+import Link from ‘next/link’;
+
+export default function Footer() {
+return (
+<footer>
+<div className="ft">
+<div>
+<div className="fln">Yorkstn</div>
+<div className="fls">Community Before Commerce</div>
+<p className="fd2">A master franchise and distribution partner for Japanese premium brands entering India. Gurugram, Delhi NCR.</p>
+</div>
+<div>
+<p className="fct">Navigate</p>
+<ul className="fli">
+<li><Link href="/">Home</Link></li>
+<li><Link href="/services">What We Do</Link></li>
+<li><Link href="/india">India Market</Link></li>
+<li><Link href="/insights">Insights</Link></li>
+<li><Link href="/about">About</Link></li>
+</ul>
+</div>
+<div>
+<p className="fct">Services</p>
+<ul className="fli">
+<li><Link href="/services">Feasibility Report</Link></li>
+<li><Link href="/services">Legal & Compliance</Link></li>
+<li><Link href="/services">Retail & Store Setup</Link></li>
+<li><Link href="/services">Operations</Link></li>
+</ul>
+</div>
+<div>
+<p className="fct">Connect</p>
+<ul className="fli">
+<li><a href="mailto:connect@yorkstn.com">connect@yorkstn.com</a></li>
+<li><Link href="/contact">Contact Page</Link></li>
+</ul>
+<br />
+<p className="fct">Location</p>
+<p style={{ fontSize: 11, color: ‘rgba(240,232,224,.28)’, fontWeight: 300, lineHeight: 2 }}>
+Gurugram, Haryana<br />Delhi NCR, India
+</p>
+</div>
+</div>
+<div className="fb">
+<p className="fcp">© 2025 Yorkstn. All rights reserved.</p>
+<p className="fcp">Founded by Madhav Sharma & Vedika Bhardwaj</p>
+</div>
+</footer>
+);
+}
